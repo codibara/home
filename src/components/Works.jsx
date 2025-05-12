99f44128684dbd7f4d06aaf4e68fee1b77007671
nosora0422 <142563767+nosora0422@users.noreply.github.com>
2025-05-12 17:47:11 +0900

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,14 +4,16 @@ import CardCarousel from './UI/Carousel'
 
 export default function Works(){
     return(
-        <div className='w-full max-w-[1440px] flex flex-col items-center py-20 mx-auto' id='works'>
-            <div className='w-full flex flex-col md:flex-row md:justify-between px-2'>
-                <div className='font-chivo text-subcolor text-[36vw] md:text-[16vw] xl:text-[230px] leading-none tracking-tight'>What</div>
-                <div className='font-chivo text-subcolor text-[36vw] md:text-[16vw] xl:text-[230px] leading-none tracking-tight text-right md:text-left -my-10 md:my-0'>We</div>
-                <div className='font-chivo text-subcolor text-[36vw] md:text-[16vw] xl:text-[230px] leading-none tracking-tight'>Did</div>
-            </div>
-            <div className='w-full flex flex-col  px-4'>
-                <CardCarousel />
+        <div className='w-full grid-background'>
+            <div className='w-full max-w-[1440px] flex flex-col items-center py-20 lg:py-32 mx-auto' id='works'>
+                <div className='w-full flex flex-col md:flex-row md:justify-between px-2'>
+                    <div className='font-chivo text-subcolor text-[36vw] md:text-[16vw] xl:text-[230px] leading-none tracking-tight'>What</div>
+                    <div className='font-chivo text-subcolor text-[36vw] md:text-[16vw] xl:text-[230px] leading-none tracking-tight text-right md:text-left -my-10 md:my-0'>We</div>
+                    <div className='font-chivo text-subcolor text-[36vw] md:text-[16vw] xl:text-[230px] leading-none tracking-tight'>Did</div>
+                </div>
+                <div className='w-full lg:w-11/12 lg:max-w-[1070px] flex flex-col px-4'>
+                    <CardCarousel />
+                </div>
             </div>
         </div>
     )
